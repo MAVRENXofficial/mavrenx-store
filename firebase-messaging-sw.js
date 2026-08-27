7d2185de5f2afe14f315c9204158d97f888bfb93
mavrenxstore <320072144+MAVRENXofficial@users.noreply.github.com>
2026-08-27 05:00:33 +0300

    Refactor Firebase initialization and messaging setup

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,14 +6,151 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
 );
 
+
 firebase.initializeApp({
-  apiKey: "AIzaSyCSXAkvMhLGaJMBwGGY1SYqoDhulIMn5F4",
-  authDomain: "mavrenxsecurity.firebaseapp.com",
-  projectId: "mavrenxsecurity",
-  storageBucket: "mavrenxsecurity.firebasestorage.app",
-  messagingSenderId: "351069637181",
-  appId: "1:351069637181:web:d204c4904017679506b854",
-  measurementId: "G-MLY1MC2SDR"
+
+  apiKey:
+    "AIzaSyCSXAkvMhLGaJMBwGGY1SYqoDhulIMn5F4",
+
+  authDomain:
+    "mavrenxsecurity.firebaseapp.com",
+
+  projectId:
+    "mavrenxsecurity",
+
+  storageBucket:
+    "mavrenxsecurity.firebasestorage.app",
+
+  messagingSenderId:
+    "351069637181",
+
+  appId:
+    "1:351069637181:web:d204c4904017679506b854",
+
+  measurementId:
+    "G-MLY1MC2SDR"
+
 });
 
-const messaging = firebase.messaging();
+
+const messaging =
+  firebase.messaging();
+
+
+/* =========================================
+   BACKGROUND NOTIFICATIONS
+========================================= */
+
+messaging.onBackgroundMessage(
+  function(payload) {
+
+    console.log(
+      "MAVRENX background notification:",
+      payload
+    );
+
+  }
+);
+
+
+/* =========================================
+   NOTIFICATION CLICK
+========================================= */
+
+self.addEventListener(
+  "notificationclick",
+
+  function(event) {
+
+    event.notification.close();
+
+
+    const notificationData =
+      event.notification.data ||
+      {};
+
+
+    const fcmMessage =
+      notificationData.FCM_MSG ||
+      {};
+
+
+    const targetUrl =
+
+      notificationData.trackingUrl ||
+
+      notificationData.url ||
+
+      fcmMessage.data?.trackingUrl ||
+
+      fcmMessage.data?.url ||
+
+      fcmMessage.fcmOptions?.link ||
+
+      "https://mavrenxofficial.github.io/mavrenx-store/";
+
+
+    event.waitUntil(
+
+      clients
+        .matchAll({
+
+          type:
+            "window",
+
+          includeUncontrolled:
+            true
+
+        })
+
+        .then(
+
+          async function(windowClients) {
+
+            for (
+              const client of windowClients
+            ) {
+
+              if (
+                "focus" in client
+              ) {
+
+                await client.focus();
+
+
+                if (
+                  "navigate" in client
+                ) {
+
+                  await client.navigate(
+                    targetUrl
+                  );
+
+                }
+
+
+                return;
+              }
+
+            }
+
+
+            if (
+              clients.openWindow
+            ) {
+
+              return clients.openWindow(
+                targetUrl
+              );
+
+            }
+
+          }
+
+        )
+
+    );
+
+  }
+
+);
